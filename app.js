7e560aca936cdb70495942c1c09653afdf330474
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-05-31 22:39:56 +0400

    a

---
+++ app.js
@@ -0,0 +1,12 @@
+// javascript code start
+
+// page not found alert_start
+// function for message
+//  function func_nullpage() {
+//      // message user: page not found 
+//     // this is my ayNet engine for the internet
+//     alert ('Cannot find the page or document! errTXT:nullPage_err')
+// }
+// page not found alert_end
+
+// javascript code end
