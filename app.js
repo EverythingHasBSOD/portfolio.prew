23b0a60495c52edb961097da716b58c67d74829d
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-06-21 22:09:08 +0400

    update all and files

--- app.js
+++ app.js
@@ -2,11 +2,16 @@
 
 // page not found alert_start
 // function for message
-//  function func_nullpage() {
-//      // message user: page not found 
-//     // this is my ayNet engine for the internet
-//     alert ('Cannot find the page or document! errTXT:nullPage_err')
-// }
-// page not found alert_end
+// function func_nullpage() {
+//       // message user: page not found 
+//      alert ('Cannot find the page or document! errTXT:nullPage_err')
+//  }
+ // page not found alert_end
+ // no instance notify alert start
+//  function instance_notfound() {
+//       // message user: instance is missing
+//      alert ('Cannot find the desired instance errTXT:instance_notfound')
+//  }
+ // no instance notify alert end
 
 // javascript code end
